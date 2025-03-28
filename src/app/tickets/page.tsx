f78dce7fd2bc60f8c62a4d1ed8a5b47f9e2e2310
Bentley Minwa <bentleyminwa@gmail.com>
2025-03-28 10:35:53 +0300

    Added ticket creation form and functionality

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,7 @@
+import { CardCompact } from "@/components/custom/card-compact";
 import { Heading } from "@/components/custom/heading";
 import { Spinner } from "@/components/custom/spinner";
+import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { Suspense } from "react";
 
@@ -7,6 +9,13 @@ const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All of your tickets in one place" />
+
+      <CardCompact
+        title="Create a new Ticket"
+        description="A new ticket will be created"
+        content={<TicketCreateForm />}
+        className="w-full max-w-[420px] self-center"
+      />
 
       <Suspense fallback={<Spinner />}>
         <TicketList />
